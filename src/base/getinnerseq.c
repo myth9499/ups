de9db84f4756f337977e045a3eb1d6c26bc1b537
李磊 <myth9499@gmail.com>
2014-10-21 22:38:26 +0800

    增加获取全局跟踪号函数

---
+++ src/base/getinnerseq.c
@@ -0,0 +1,50 @@
+#include  "ups.h"
+#include  "pool.h"
+#include  <sys/file.h>
+
+/** 获取交易跟踪号 **/
+
+long getinnerid()
+{
+
+#ifdef lilei
+        FILE    *fp ;
+        char    filepath[100];
+        char    str[100];
+		long	ret=0;
+        memset(filepath,0,sizeof(filepath));
+        memset(str,0,sizeof(str));
+
+        strcpy(filepath,"/item/ups/etc/innerid.seq");
+        fp = fopen(filepath,"r+");
+        if(fp == NULL)
+        {
+			SysLog(1,"FILE [%s] LINE [%d]:打开seq文件失败:%s\n",__FILE__,__LINE__,strerror(errno));
+            return  -1L;
+        }
+		if(flock(fileno(fp),LOCK_EX)==0)
+		{
+			memset(str,0x00,sizeof(str));
+			if(fgets(str,sizeof(str),fp)==(void *)-1)
+			{
+				SysLog(1,"FILE [%s] LINE [%d]:获取SEQ失败:%s\n",__FILE__,__LINE__,strerror(errno));
+				sprintf(str,"%8ld",ret);
+				fwrite(str,strlen(str),1,fp);
+				flock(fileno(fp), LOCK_UN);
+				fclose(fp);
+				return 1L;
+			}
+			ret = atol(str);
+			ret = ret++;
+			memset(str,0x00,sizeof(str));
+			sprintf(str,"%8ld",ret);
+			fwrite(str,strlen(str),1,fp);
+			flock(fileno(fp), LOCK_UN);
+        	fclose(fp);
+			return ret ;
+		}
+        fclose(fp);
+        return -1L;
+#endif
+		return	globalinnerid++;
+}
